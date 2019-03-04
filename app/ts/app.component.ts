9fc95f364054d2d14ef909c9527b96ab0790d944
Hyodae <hyodaekim@gmail.com>
2019-03-04 14:20:07 +0900

    first commit

---
+++ app/ts/app.component.ts
@@ -0,0 +1,25 @@
+import {Component} from 'angular2/core';
+import {Config} from "./config.service";
+import {Video} from "./video";
+import {PlaylistComponent} from "./playlist.component";
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
+})
+
+export class AppComponent {
+    mainHeading = Config.MAIN_HEADING;
+    videos:Array<Video>;
+
+    constructor(){
+        this.videos = [
+            new Video(1, "Nested Components", "f8qBeaGe2S4", "Angular 2 for Beginners - Tutorial 7 - Nested Components" ),
+            new Video(2, "Passing Data to Components", "bKWDKmHvF78", "Angular 2 for Beginners - Tutorial 8 - Passing Data to Components" ),
+        ]
+    }
+}
+
+
+
